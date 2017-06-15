026da9dc97458ae556cab35e1f231760d87152f7
Xiaochuan Wang <xiaochuan.wang02@sap.com>
2017-06-15 13:06:28 +0800

    add proxy setting in wsgi

--- djangoproject/wsgi.py
+++ djangoproject/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import getpass
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoproject.settings")
+
+if getpass.getuser() == "I321761":
+    print("! Office environment, set http proxy")
+    os.environ['http_proxy'] = "http://proxy.pal.sap.corp:8080"
+    os.environ['https_proxy'] = "http://proxy.pal.sap.corp:8080"
 
 application = get_wsgi_application()
