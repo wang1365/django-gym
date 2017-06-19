479a1fb5bbf4712729e6212cbeb142219b65d484
Xiaochuan Wang <xiaochuan.wang02@sap.com>
2017-06-19 17:20:37 +0800

    add migration summary

--- firstapp/models.py
+++ firstapp/models.py
@@ -5,6 +5,10 @@ from django.db import models
 
 # Create your models here.
 
+# If you want implement model changes to db, use: python manage.py migrate app
+# If you want to store model's change, use: python manage.py makemigrations app
+# If you want to view detail of one stored migration, use: python manage.py sqlmigrate app 000x
+# If you want to implement one stored migration, use: python manage.py migrate app 000x
 
 class Country(models.Model):
     id = models.IntegerField(auto_created=True, primary_key=True)
@@ -16,5 +20,5 @@ class User(models.Model):
     name = models.CharField(max_length=128)
     age = models.IntegerField()
     country_id = models.ForeignKey(Country, on_delete=models.CASCADE)
-    description = models.CharField(max_length=512)
+    description = models.CharField(max_length=512, null=True)
 
